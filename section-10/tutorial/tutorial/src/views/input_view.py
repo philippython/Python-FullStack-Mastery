2255c0654db1536893373420b8ce0c0db08d4385
Odulaja Philip <philip.odulaja@nexaneon.com>
2025-10-20 00:42:25 +0100

    Section 11 completed

--- section-10/tutorial/tutorial/src/views/input_view.py
+++ section-10/tutorial/tutorial/src/views/input_view.py
@@ -3,7 +3,7 @@ from flet import *
 def input_view():
     return View(
         route="/forms",
-        bgcolor=Colors.random(),
+        bgcolor=Colors.WHITE,
         scroll=ScrollMode.ALWAYS,
         controls=[
             AutoComplete(
@@ -40,8 +40,72 @@ def input_view():
             ),
             Checkbox(
                 active_color=Colors.RED,
-                borde,
-                border = border.all(5, Colors.YELLOW)
-            )
+                check_color=Colors.BLUE,
+                hover_color=Colors.WHITE,
+                label="Are you loving this course",
+                label_position=LabelPosition.LEFT,
+                shape=CircleBorder(),
+            ),
+            RadioGroup(
+                content=Column(
+                    [
+                        Radio(value="red", label="Red", adaptive=True),
+                        Radio(value="green", label="Green", adaptive=True),
+                        Radio(value="blue", label="Blue", adaptive=True),
+                    ]
+                ),
+            ),
+            Dropdown(
+                bgcolor=Colors.GREY,
+                color=Colors.GREEN,
+                enable_search=True,
+                editable=True,
+                label="Cities",
+                options= [
+                    DropdownOption(
+                        content=Text("Tokyo"),
+                        key="Tokyo"
+                    ),
+                    DropdownOption(
+                        content=Text("Yamanshi"),
+                        key="Yamanshi"
+                    ),
+                    DropdownOption(
+                        content=Text("Kofu"),
+                        key="Kofu"
+                    ),
+                    DropdownOption(
+                        content=Text("Osaka"),
+                        key="Osaka"
+                    ),
+                    DropdownOption(
+                        content=Text("Ogun"),
+                        key="Ogun"
+                    )
+                ]
+            ),
+            RangeSlider(
+                min=0,
+                max=50,
+                start_value=10,
+                end_value=20
+            ),
+            SearchBar(
+                autofocus=True,
+                bar_bgcolor=Colors.WHITE,
+                bar_hint_text="Search items..."
+            ),
+            Text("Slider with value:"),
+            Slider(value=0.3),
+            Text("Slider with a custom range and label:"),
+            Slider(min=0, max=100, divisions=10, label="{value}%"),
+            Switch(label="Unchecked switch", value=False, active_color=Colors.BLUE_400, adaptive=True),
+            Switch(label="Checked switch", value=True, active_color=Colors.BLUE_400),
+            Switch(label="Disabled switch", disabled=True, active_color=Colors.BLUE_400),
+            TextField(label="Standard"),
+            TextField(label="Disabled", disabled=True, value="First name"),
+            TextField(label="Read-only", read_only=True, value="Last name"),
+            TextField(label="With placeholder", hint_text="Please enter text here"),
+            TextField(label="With an icon", icon=Icons.EMOJI_EMOTIONS)
         ]
     )
